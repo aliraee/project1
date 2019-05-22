166d84a0aa70fccde847ef5fa9434516387bf96d
mahdiba99 <mahdi.bakhshali1999@gamil.com>
2019-05-22 12:02:22 +0430

    finite automata class initialzer

--- finiteautomata.py
+++ finiteautomata.py
@@ -1,1 +1,8 @@
-#hello world
+class finite_automata:
+    def __init__(self, states = [], alphabets = [], transistions = {}, init_state = 'q0', final_states = []):
+        self.states = states
+        self.alphabets = alphabets
+        self.transitions = transistions
+        self.init_state = init_state
+        self.final_states = final_states
+    
